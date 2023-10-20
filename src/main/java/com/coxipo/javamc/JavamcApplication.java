4e198671dec13552e9293c4b8a3b1df467dc73b1
Wagner Couto <wagnereler@hotmail.com>
2023-10-19 22:14:07 -0300

    Projeto Criado

---
+++ src/main/java/com/coxipo/javamc/JavamcApplication.java
@@ -0,0 +1,13 @@
+package com.coxipo.javamc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavamcApplication.class, args);
+	}
+
+}
